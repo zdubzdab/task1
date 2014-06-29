0ccd7d29a58a89d7dce58d7881b358998958b039
zdubzdab <zdubzdab@gmail.com>
2014-06-29 13:16:37 +0300

    val

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,11 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true,
+
                     length: { minimum: 3 },
                     format: { with: /[a-z]/i,
                     message: " allows only letters" },
                     uniqueness: true
+                    length: { minimum: 3 }
+
   validates :text, presence: true
 end
