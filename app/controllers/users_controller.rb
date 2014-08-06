ca8763477e10b1c727d398718a4e23c6493665d1
zdubzdab <zdubzdab@gmail.com>
2014-08-06 14:41:13 +0300

    Welcome email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      UserMailer.welcome_email(@user).deliver
       redirect_to users_path
     else
       render 'new'
