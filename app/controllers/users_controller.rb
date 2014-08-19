a6a942c142c9acd17bc350fa213e5e1842d1545d
zdubzdab <zdubzdab@gmail.com>
2014-08-19 14:35:42 +0300

    Add_user_without_reload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,18 @@ class UsersController < ApplicationController
   
   def index 
     @users = User.all
-
   end
 
   def create
     @user = User.new(user_params)
-
-    if @user.save
-      
-      redirect_to users_path
-    else
-      render 'new'
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_url, notice: 'User was successfully created.' }
+        format.js  
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -29,16 +30,14 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
- 
     if @user.update(user_params)
-
       redirect_to user_path
     else
       render 'edit'
     end
   end
 
-   def email
+  def email
     @user = User.find(params[:id])
     UserMailer.welcome_email(@user).deliver
     redirect_to users_path
@@ -47,7 +46,6 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
- 
     redirect_to users_path
   end
 
@@ -55,5 +53,4 @@ private
   def user_params
     params.require(:user).permit(:name, :email, :id)
   end
-
 end
