4ffc55c69bd21dc479bf6ce2292713636aadf274
zdubzdab <zdubzdab@gmail.com>
2014-11-12 14:30:24 +0200

    successful_sign_in_with_email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014164020) do
+ActiveRecord::Schema.define(version: 20141112105643) do
 
   create_table "articles", force: true do |t|
     t.integer  "user_id"
@@ -54,8 +54,12 @@ ActiveRecord::Schema.define(version: 20141014164020) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["username"], name: "index_users_on_username", unique: true
 
