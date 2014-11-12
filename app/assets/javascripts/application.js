225cd6a1abc9e49b3988606b8ec08aefb50eed0b
zdubzdab <zdubzdab@gmail.com>
2014-11-12 14:52:09 +0200

    some

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,9 @@
 // about supported directives.
 //
 
-
+//= require twitter/bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
 
