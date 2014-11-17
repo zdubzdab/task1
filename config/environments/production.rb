9cb9e894b95b46c497a062a30e234c1250ddff07
zdubzdab <zdubzdab@gmail.com>
2014-11-17 12:19:11 +0200

    receive confirmation email2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Task1::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'www.example.com' }
+  config.action_mailer.default_url_options = { host: 'http://zuzu.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { 
