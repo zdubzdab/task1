0ab2fa7cfaed4a68ef2f57ccf822dd6b85b9b2b5
zdubzdab <zdubzdab@gmail.com>
2014-12-18 14:16:17 +0200

    kdiff3 after corrections

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,9 +7,4 @@ class UserMailer < ActionMailer::Base
     mail(to: @user.email, subject: 'Welcome to ')
   end
 
-  def password_changed id
-    @user = User.find(id)
-    mail to: @user.email, subject: "Your password has changed"
-  end
-
 end
