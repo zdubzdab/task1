340a169f9756d8a93b464e3b9e816115367a742b
zdubzdab <zdubzdab@gmail.com>
2014-12-21 17:52:19 +0200

    g

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,12 +90,4 @@ Task1::Application.configure do
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
   }
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
 end
