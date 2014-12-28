520c62e46c3c8ff76ceaa09fca310d8915b235d4
zdubzdab <zdubzdab@gmail.com>
2014-12-28 19:44:53 +0200

    send email to chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,8 @@ class User < ActiveRecord::Base
     end
   end
   
+def confirmation_required?
+  false
+end
+
 end
--- app/views/users/update.js
+++
@@ -1,3 +0,0 @@
-$('#new_task').remove();
-$('#new_link').show();
-$("<%= escape_javascript(render @user) %>").appendTo("#table");
